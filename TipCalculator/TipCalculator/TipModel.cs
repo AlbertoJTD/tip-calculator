6f80670db95941045dbca694346e599de4bf63ac
AlbertoJTD <albertojtd@outlook.com>
2023-07-04 11:44:24 -0600

    Add Tip model

---
+++ TipCalculator/TipCalculator/TipModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalculator
+{
+    public class TipModel
+    {
+        public decimal Total { get; set; }
+        public int PercentageTip { get; set; }
+        public int NumberPeople { get; set; }
+        public decimal TotalTip { get; set; }
+        public decimal TotalBill { get; set; }
+        public decimal TipPerPerson { get; set; }
+        public decimal TotalTipPerPerson { get;set; }
+    }
+}
