711abe40f503c8dd8513e5f98a3706b636b3bbef
AlbertoJTD <albertojtd@outlook.com>
2023-07-04 11:54:19 -0600

    Replace values with data binding

--- TipCalculator/TipCalculator/MainPage.xaml.cs
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -10,9 +10,19 @@ namespace TipCalculator
 {
 	public partial class MainPage : ContentPage
 	{
+		private TipModel tipModel;
+
 		public MainPage()
 		{
 			InitializeComponent();
+
+			tipModel = new TipModel
+			{
+				PercentageTip = 5,
+				NumberPeople = 2,
+			};
+
+			this.BindingContext = tipModel;
 		}
 
 		private void CalculateButton_Clicked(object sender, EventArgs e)
