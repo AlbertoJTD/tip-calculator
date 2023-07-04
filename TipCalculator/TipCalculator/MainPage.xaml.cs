8dc150fd6b4b20f517cc43cf1cdc846b820499b4
AlbertoJTD <albertojtd@outlook.com>
2023-07-04 14:29:14 -0600

    Add commands and INotifyPropertyChanged

--- TipCalculator/TipCalculator/MainPage.xaml.cs
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -24,13 +24,5 @@ namespace TipCalculator
 
 			this.BindingContext = tipModel;
 		}
-
-		private void CalculateButton_Clicked(object sender, EventArgs e)
-		{
-			tipModel.TotalTip = tipModel.Total * (tipModel.PercentageTip / 100);
-			tipModel.TotalBill = tipModel.Total + tipModel.TotalTip;
-			tipModel.TipPerPerson = tipModel.TotalTip / tipModel.NumberPeople;
-			tipModel.TotalPerPerson = tipModel.TotalBill / tipModel.NumberPeople;
-		}
 	}
 }
--- TipCalculator/TipCalculator/TipModel.cs
+++ TipCalculator/TipCalculator/TipModel.cs
@@ -1,17 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using Xamarin.Forms;
 
 namespace TipCalculator
 {
-    public class TipModel
+    public class TipModel: INotifyPropertyChanged
     {
-        public decimal Total { get; set; }
+		private decimal totalPerPerson;
+		private decimal totalTip;
+		private decimal totalBill;
+		private decimal tipPerPerson;
+
+		public decimal Total { get; set; }
         public int PercentageTip { get; set; }
         public int NumberPeople { get; set; }
-        public decimal TotalTip { get; set; }
-        public decimal TotalBill { get; set; }
-        public decimal TipPerPerson { get; set; }
-        public decimal TotalPerPerson { get;set; }
-    }
+		public decimal TotalTip
+		{
+			get { return totalTip; }
+			set
+			{
+				totalTip = value;
+				OnPropertyChanged();
+			}
+		}
+		public decimal TotalBill
+		{
+			get { return totalBill; }
+			set
+			{
+				totalBill = value;
+				OnPropertyChanged();
+			}
+		}
+		public decimal TipPerPerson
+		{
+			get { return tipPerPerson; }
+			set
+			{
+				tipPerPerson = value;
+				OnPropertyChanged();
+			}
+		}
+		public decimal TotalPerPerson
+		{
+			get { return totalPerPerson;}
+			set {
+				totalPerPerson = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+		public Command OperationsCommand { get; set; }
+
+        public TipModel()
+        {
+            OperationsCommand = new Command(Operations);
+		}
+
+		private void Operations()
+        {
+			TotalTip = Total * ((decimal)PercentageTip / 100);
+			TotalBill = Total + TotalTip;
+			TipPerPerson = TotalTip / NumberPeople;
+			TotalPerPerson = TotalBill / NumberPeople;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs((propertyName)));
+		}
+	}
 }
