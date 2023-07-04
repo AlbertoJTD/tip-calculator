734ca36e718f5c41ddb94bc4f93624979691a60e
AlbertoJTD <albertojtd@outlook.com>
2023-07-03 20:44:16 -0600

    Add logic to calculate tip

--- TipCalculator/TipCalculator/MainPage.xaml.cs
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -14,5 +14,19 @@ namespace TipCalculator
 		{
 			InitializeComponent();
 		}
+
+		private void CalculateButton_Clicked(object sender, EventArgs e)
+		{
+			var total = (decimal.Parse(inputTotal.Text));
+			var tip = (decimal.Parse(inputPercentageTip.Text));
+			var numberPeople = (decimal.Parse(inputNumberPeople.Text));
+
+			var totalTip = total * (tip / 100);
+
+			labelTip.Detail = totalTip.ToString("C");
+			labelTotal.Detail = (total + totalTip).ToString("C");
+			labelTipPerPerson.Detail = (totalTip / numberPeople).ToString("C");
+			labelTotalPerPerson.Detail = ((total + totalTip) / numberPeople).ToString("C");
+		}
 	}
 }
